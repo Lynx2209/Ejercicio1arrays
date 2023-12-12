64949aa8d2b86c5685132e77ee55c56203686d40
Lynx2209 <csi23-jconazo@colegioaltair.net>
2023-12-12 14:19:08 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Ejercici1array
+
+{
+    class program
+    {
+        public static void Main(String[] args)
+        {
+            char[] VocalesUno = { 'a', 'e', 'i', 'o', 'u' };
+            char[] VocalesDos = { 'a', 'e', 'i', 'o', 'x' };
+
+            for (int recorrido = 0; recorrido < 5; recorrido++)
+            {
+                if (VocalesUno[recorrido] != VocalesDos[recorrido])
+                {
+                    Console.WriteLine("En la posicion: " + recorrido + " del segundo array esta mal");
+                }
+            }
+        }
+    }
+}
